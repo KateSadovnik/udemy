df343d81bd209462d72975127c2a4a58c5ed9c8f
Katya Sadovnikova <katya.sadovnikova@activeplatform.com>
2020-11-28 11:39:34 +0300

    scaffold crud articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_172916) do
+ActiveRecord::Schema.define(version: 2020_11_26_180646) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
